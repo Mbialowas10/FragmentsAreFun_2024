4a2994ec041c63338ef73f5a99d647c0e3080b06
Mike bialowas <mikebialowas@Mikes-MBP.wp.shawcable.net>
2024-09-05 10:40:44 -0500

    implemented bottom navigation with fragments

--- app/src/main/java/com/mbialowas/fragmentsarefun_2024/MainActivity.kt
+++ app/src/main/java/com/mbialowas/fragmentsarefun_2024/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.mbialowas.fragmentsarefun_2024
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavBar: BottomNavigationView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // promise
+        bottomNavBar = findViewById(R.id.bottom_nav)
+
+        bottomNavBar.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.home_nav -> replaceFragment(HomeFragment())
+                R.id.search_nav -> replaceFragment(SearchFragment())
+                R.id.profile_nav -> replaceFragment(ProfileFragment())
+            }
+            true
+
+
+        }
+    }
+    private fun replaceFragment(fragment: Fragment): Boolean {
+        Log.i("mjb", "fragment method being called")
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+        return true
     }
 }
